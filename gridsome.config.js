bc450619c72d4f955d53dd73713b304f315c8a90
ishan masdekar <imasdekar@dff.org.in>
2020-04-11 00:03:10 +0530

    SITE REVAMP USING GRIDSOME
    - Revamps the site with gridsome DOCC template.
    
    Signed-off by: Ishan Masdekar<imasdekar@disroot.org>

---
+++ gridsome.config.js
@@ -0,0 +1,90 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'ITcracy',
+  icon: {
+    favicon: './src/assets/favicon.png',
+    touchicon: './src/assets/favicon.png'
+  },
+  siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : 'https://example.com'),
+  settings: {
+    gitlab : process.env.URL_GITLAB || true,
+    github: process.env.URL_GITHUB || true,
+    nav: {
+      links: [
+        { path: '/about/', title: 'About' }
+      ]
+    },
+    sidebar: [
+      {
+        name: 'about',
+        sections: [
+          {
+            title: 'Getting Started',
+            items: [
+              '/about/',
+              '/about/installation/',
+              '/about/writing-content/',
+              '/about/deploying/',
+            ]
+          },
+          {
+            title: 'Configuration',
+            items: [
+              '/about/settings/',
+              '/about/sidebar/',
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        baseDir: './content',
+        path: '**/*.md',
+        typeName: 'MarkdownPage',
+        remark: {
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['noopener', 'noreferrer'],
+          plugins: [
+            '@gridsome/remark-prismjs'
+          ]
+        }
+      }
+    },
+
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {
+          // Prevent purging of prism classes.
+          whitelistPatternsChildren: [
+            /token$/
+          ]
+        }
+      }
+    },
+
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: (process.env.GA_ID ? process.env.GA_ID : 'XX-999999999-9')
+      }
+    },
+
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+      }
+    }
+
+  ]
+}
---
+++ src/components/LayoutHeader.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="py-2 border-t-2 border-ui-primary">
+    <div class="container">
+
+      <div class="flex items-center justify-between -mx-2 sm:-mx-4">
+        <div class="flex flex-col items-center px-2 mr-auto sm:px-4 sm:flex-row">
+          <g-link
+            to="/"
+            class="flex items-center text-ui-primary"
+            title="Home"
+          >
+            <Logo :width="60" class="text-ui-primary" />
+            <span class="hidden ml-2 text-xl font-black tracking-tighter uppercase sm:block">
+              {{ meta.siteName }}
+            </span>
+          </g-link>
+
+          <div v-if="settings.nav.links.length > 0" class="hidden ml-2 mr-5 sm:block sm:ml-8">
+            <g-link
+              v-for="link in settings.nav.links"
+              :key="link.path"
+              :to="link.path"
+              class="block p-1 font-medium nav-link text-ui-typo hover:text-ui-primary"
+            >
+              {{ link.title }}
+            </g-link>
+          </div>
+        </div>
+
+        <div class="w-full px-2 sm:px-4 max-w-screen-xs">
+          <ClientOnly>
+            <Search />
+          </ClientOnly>
+        </div>
+
+        <div class="flex items-center justify-end px-2 sm:px-4">
+
+          <a v-if="settings.github" :href="settings.github" class="sm:ml-3" target="_blank" rel="noopener noreferrer" title="Github" name="Github">
+            <GithubIcon size="1.5x" />
+          </a>
+          <a v-if="settings.gitlab" :href="settings.gitlab" class="sm:ml-3" target="_blank" rel="noopener noreferrer" title="Gitlab" name="Gitlab">
+            <GitlabIcon size="1.5x" />
+          </a>
+
+          <ToggleDarkMode class="ml-2 sm:ml-8">
+            <template slot="default" slot-scope="{ dark }">
+              <CoffeeIcon v-if="dark" size="1.5x" />
+              <SunIcon v-else size="1.5x" />
+            </template>
+          </ToggleDarkMode>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    settings {
+      github
+      gitlab
+      nav {
+        links {
+          path
+          title
+        }
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+import ToggleDarkMode from "@/components/ToggleDarkMode";
+import Logo from '@/components/Logo';
+import { SunIcon, CoffeeIcon, GithubIcon, GitlabIcon } from "vue-feather-icons";
+
+const Search = () => import(/* webpackChunkName: "search" */ "@/components/Search").catch(error => console.warn(error));
+
+export default {
+  components: {
+    Logo,
+    Search,
+    ToggleDarkMode,
+    SunIcon,
+    CoffeeIcon,
+    GithubIcon,
+    GitlabIcon,
+
+  },
+
+  computed: {
+    meta() {
+      return this.$static.metadata;
+    },
+    settings() {
+      return this.meta.settings;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+header {
+  svg:not(.feather-search) {
+    &:hover {
+      @apply text-ui-primary;
+    }
+  }
+}
+
+.nav-link {
+  &.active {
+    @apply text-ui-primary font-bold border-ui-primary;
+  }
+}
+</style>
